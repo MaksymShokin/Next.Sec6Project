d90f0ac9f518638b2359bdfd884d1d3578223fa8
Maksym <maksym.sh@upplabs.com>
2021-03-11 10:52:56 +0200

    Head-in-_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from '../components/layout/layout';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      {/* will be overwritten by more specific Head */}
+      <Head>
+        <title>Next Title</title>
+        <meta name='keywords' content='super keyword' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
